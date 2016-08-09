3b036d57880e12a83f66618f18fa14afb7d3b71a
rain123473 <rain123473@gmail.com>
2016-08-09 09:21:40 +0000

    core/App.php  縮排

--- core/App.php
+++ core/App.php
@@ -8,24 +8,23 @@ class App
         $url = $this->parseUrl();
 
         $controllerName = "{$url[0]}Controller";
-        
-        //如果導入為首頁，則自動導向GameController
+
+        //如果導入為首頁，則自動導向HameController
 
         if (!$url) {
             $controllerName = "HomeController";
         }
-        if (!file_exists("controllers/$controllerName.php")) {
 
+        if (!file_exists("controllers/$controllerName.php")) {
             return;
- 
         }
+
         require_once "controllers/$controllerName.php";
         $controller = new $controllerName;
         $methodName = isset($url[1]) ? $url[1] : "index";
+
         if (!method_exists($controller, $methodName)) {
-
             return;
-
         }
         unset($url[0]); unset($url[1]);
         $params = $url ? array_values($url) : Array();
@@ -33,12 +32,11 @@ class App
     }
 
     public function parseUrl() {
-        if (isset($_GET["url"])) 
-        {
+        if (isset($_GET["url"])) {
             $url = rtrim($_GET["url"], "/");
             $url = explode("/", $url);
+
             return $url;
         }
     }
-
 }
