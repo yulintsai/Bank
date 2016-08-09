a0410c850c14c0dc065d05ee3241d204624fe582
rain123473 <rain123473@gmail.com>
2016-08-09 04:13:55 +0000

    調整index縮排 刪除session_star

--- index.php
+++ index.php
@@ -1,9 +1,10 @@
 <?php
-header("Content-Type:text/html; charset=utf-8");
 require_once 'core/App.php';
 require_once 'core/Controller.php';
 require_once "core/Server.php";
+
+header("Content-Type:text/html; charset=utf-8");
 date_default_timezone_set('Asia/Taipei');
-session_start();
+
 $app = new App();
 
