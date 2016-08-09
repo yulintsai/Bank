640da314a63aff7899c51498361c965f9400e69b
rain123473 <rain123473@gmail.com>
2016-08-09 12:32:03 +0000

    修改showDetails的顯示ID方法

--- views/showDetails.php
+++ views/showDetails.php
@@ -2,11 +2,10 @@
     echo "<table border=1px><tr><td>ID</td><td>Time</td><td>Dispense</td><td>Deposit</td>
           <td>Balance</td><td>Remark</td></tr>";
     foreach ($data as $Num => $ID) {
-        echo "<tr>";
+        echo "<tr><td>".($Num+1)."</td>";
         foreach ($ID as $key => $value) {
             echo "<td>$value</td>";
         }
         echo "</tr>";
     }
     echo "</table>";
-    
