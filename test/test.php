cf4bc54d7fc296bd827dc94c33987b6335297101
rain123473 <rain123473@gmail.com>
2016-08-10 05:47:20 +0000

    完成基本測試function

--- test/test.php
+++ test/test.php
@@ -6,6 +6,21 @@ class BankTest extends HomeController
 {
     public function testInsertAccountDispense()
     {
-        $this->insertAccountDispense(200, "撿到");
+        $this->insertAccountDispense(500, "保險");
+    }
+
+    public function testInsertAccountDeposit()
+    {
+        $this->insertAccountDeposit(30000, "薪水");
+    }
+
+    public function testSearchAccountBalance()
+    {
+        $this->searchAccountBalance();
+    }
+
+    public function testSearchAccountDetails()
+    {
+        $this->searchAccountDetails();
     }
 }
