18d2c6ba5276089ea5da1e3731e71efb7cb2a257
rain123473 <rain123473@gmail.com>
2016-08-10 03:51:59 +0000

    views/showDetails 修改變數名稱

--- views/showDetails.php
+++ views/showDetails.php
@@ -1,9 +1,9 @@
 <?php
     echo "<table border=1px><tr><td>ID</td><td>Time</td><td>Dispense</td><td>Deposit</td>
           <td>Balance</td><td>Remark</td></tr>";
-    foreach ($data as $Num => $ID) {
+    foreach ($data as $num => $id) {
         echo "<tr><td>".($Num+1)."</td>";
-        foreach ($ID as $key => $value) {
+        foreach ($id as $key => $value) {
             echo "<td>$value</td>";
         }
         echo "</tr>";
