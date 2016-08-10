6613c57adbaa1e4472997a5c3ea4db576281d3bd
rain123473 <rain123473@gmail.com>
2016-08-10 03:52:42 +0000

    index.php 修改雙引號為單引號

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 require_once 'core/App.php';
 require_once 'core/Controller.php';
-require_once "core/Server.php";
+require_once 'core/Server.php';
 
 header("Content-Type:text/html; charset=utf-8");
 date_default_timezone_set('Asia/Taipei');
