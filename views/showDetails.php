fd275e041dde1a4117c821abf4067f6d1f9a7387
rain123473 <rain123473@gmail.com>
2016-08-11 03:49:02 +0000

    修改showDetails 把key變數刪除

--- views/showDetails.php
+++ views/showDetails.php
@@ -1,12 +1,13 @@
 <?php
-    echo "<table border=1px><tr><td>ID</td><td>Time</td><td>Dispense</td><td>Deposit</td>
-          <td>Balance</td><td>Remark</td></tr>";
-    foreach ($data as $num => $id) {
-        echo "<tr><td>".($num+1)."</td>";
-        foreach ($id as $key => $value) {
-            echo "<td>$value</td>";
-        }
-        echo "</tr>";
+echo "<table border=1px><tr><td>ID</td><td>Time</td><td>Dispense</td><td>Deposit</td>
+      <td>Balance</td><td>Remark</td></tr>";
+foreach ($data as $num => $id) {
+    echo "<tr><td>".($num+1)."</td>";
+
+    foreach ($id as $value) {
+        echo "<td>$value</td>";
     }
-    echo "</table>";
-    echo "<a href='/Bank'>BACK</a>";
+    echo "</tr>";
+}
+echo "</table>";
+echo "<a href='/Bank'>BACK</a>";
