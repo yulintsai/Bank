149bf3e554a77f4feb1bc29441eac2fe2d346296
rain123473 <rain123473@gmail.com>
2016-08-12 07:51:11 +0000

    修改進入帳號方式

--- test/AccountDispenseTest.php
+++ test/AccountDispenseTest.php
@@ -8,15 +8,15 @@ class AccountDispenseTest extends \PHPUnit_Framework_TestCase
     public function testNormalDispense()
     {
         $Account = new Account();
-        $_SESSION['account']='1';
-        $result = $Account->doDispense(500, "正常領出測試");
+        $Account->intoAccount(1);
+        $result = $Account->doDispense(50, "正常領出測試");
         $this -> assertEquals("", $result);
     }
 
     public function testBust()
     {
         $Account = new Account();
-        $_SESSION['account']='1';
+        $Account->intoAccount(1);
         $result = $Account->doDispense(5000000000000, "超額領出測試");
         $this -> assertEquals("餘額不足", $result);
     }
