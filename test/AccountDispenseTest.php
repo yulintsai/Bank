7660a38eef5f3cf47fcb0c4c54e31c00326ec833
rain123473 <rain123473@gmail.com>
2016-08-12 06:31:52 +0000

    完成出款測試

--- test/AccountDispenseTest.php
+++ test/AccountDispenseTest.php
@@ -1,27 +1,23 @@
 <?php
-require_once '/home/ubuntu/workspace/Bank/index.php';
+
+require_once '/home/ubuntu/workspace/Bank/core/Server.php';
 require_once '/home/ubuntu/workspace/Bank/models/Account.php';
 
 class AccountDispenseTest extends \PHPUnit_Framework_TestCase
 {
-    private $Account;
-
-    public function __construct()
-    {
-        $this->Account = new Account();
-    }
-
     public function testNormalDispense()
     {
+        $Account = new Account();
         $_SESSION['account']='1';
-        $reslut = $this->Account->doDispense(200, "基本領出");
+        $result = $Account->doDispense(50, "正常領出測試");
         $this -> assertEquals("", $result);
     }
 
     public function testBust()
     {
+        $Account = new Account();
         $_SESSION['account']='1';
-        $result = $this->Account->doDispense(5000000000000, "超額領出測試");
+        $result = $Account->doDispense(5000000000000, "超額領出測試");
         $this -> assertEquals("餘額不足", $result);
     }
 }
