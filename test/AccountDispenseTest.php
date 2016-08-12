c85c28dccd81c434933eb82768c616485887e009
rain123473 <rain123473@gmail.com>
2016-08-12 01:56:00 +0000

    完成出款測試碼

--- test/AccountDispenseTest.php
+++ test/AccountDispenseTest.php
@@ -1,28 +1,34 @@
 <?php
-
 require_once '/home/ubuntu/workspace/Bank/index.php';
-require_once '/home/ubuntu/workspace/Bank/controllers/HomeController.php';
+require_once '/home/ubuntu/workspace/Bank/models/Account.php';
 
 class AccountDispenseTest extends \PHPUnit_Framework_TestCase
 {
+    private $Account;
+
+    public function __construct()
+    {
+        $this->Account = new Account();
+    }
+
     public function testNormalDispense()
     {
-        $_SESSION['account']='123';
-        $Home = new HomeController();
-        $Home->insertAccountDispense(200, "基本領出");
+        $_SESSION['account']='1';
+        $reslut = $this->Account->doDispense(200, "基本領出");
+        $this -> assertEquals("", $result);
     }
 
     public function testBust()
     {
-        $_SESSION['account']='123';
-        $Home = new HomeController();
-        $Home->insertAccountDispense(50000, "超額領出測試");
+        $_SESSION['account']='1';
+        $result = $this->Account->doDispense(50000, "超額領出測試");
+        $this -> assertEquals("餘額不足", $result);
     }
 
-    public function testFalseInput()
+    public function testInputMoney()
     {
-        $_SESSION['account']='123';
-        $Home = new HomeController();
-        $Home->insertAccountDispense("yoyo",200);
+        $_SESSION['account']='1';
+        $result = $this->Account->doDispense("fwqfw", "非數字測試");
+        $this -> assertEquals("", $result);
     }
 }
