793055bf85460f97d007483e10944f09da9e97ff
rain123473 <rain123473@gmail.com>
2016-08-12 06:32:42 +0000

    新增入款測試

---
+++ test/AccountDepositTest.php
@@ -0,0 +1,24 @@
+<?php
+
+require_once '/home/ubuntu/workspace/Bank/index.php';
+require_once '/home/ubuntu/workspace/Bank/models/Account.php';
+
+class AccountDepositTest extends \PHPUnit_Framework_TestCase
+{
+    public function testNormalDeposit()
+    {
+        $Account = new Account();
+        $Account->intoAccount(1);
+        $result = $Account->doDeposit(20000, "基本入款");
+        $Account->showDetails();
+        $this -> assertEquals("", $result);
+    }
+
+    public function testNegative()
+    {
+        $Account = new Account();
+        $Account->intoAccount(1);
+        $result = $Account->doDeposit("HI", "負數測試");
+        $this -> assertEquals("金額必須為數字", $result);
+    }
+}
